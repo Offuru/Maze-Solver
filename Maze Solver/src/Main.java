2de0a0c9db6f350bb7cfbca7b1356de1dde246fd
Offuru <andrei.rozmarin@gmail.com>
2023-10-29 10:55:19 +0200

    Implemented Main class

--- Maze Solver/src/Main.java
+++ Maze Solver/src/Main.java
@@ -1,2 +1,30 @@
+import javax.swing.*;
+
 public class Main {
+
+    static JFrame frame;
+    static Panel panel;
+
+    public static void InitUI() {
+        frame = new JFrame("GraphBuilder");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel = new Panel("labyrinth.txt"));
+        frame.setSize(800, 800);
+        frame.setVisible(true);
+        frame.setFocusable(true);
+        frame.requestFocusInWindow();
+
+    }
+
+    public static void main(String[] args){
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                InitUI();
+            }
+        });
+
+    }
+
 }
